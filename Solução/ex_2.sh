a55b0b2252c12cca3080c1abe0201e549b78fd80
Vitor <vitor.mnw@gmail.com>
2023-04-03 14:42:49 -0300

    correção ex_2

--- Solução/ex_2.sh
+++ Solução/ex_2.sh
@@ -1,5 +1,5 @@
-for test in ../Dados/machado/*/*.txt; do
-    sed -n '1p' $text >> temp_res_ex_2;
+for text in ../Dados/machado/*/*.txt; do
+    sed -n '3p' $text >> temp_res_ex_2;
 done
 
 sort temp_res_ex_2 > resposta_ex_2
